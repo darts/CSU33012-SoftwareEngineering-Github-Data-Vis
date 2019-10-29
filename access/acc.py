1e72b7d35ca6fa470c1a60bfd836737088f4f25a
darts <darts@tcd.ie>
2019-10-29 20:33:14 +0000

    Now a sorted list

--- access/acc.py
+++ access/acc.py
@@ -6,21 +6,22 @@ f1 = open('commits.txt', 'w+')
 g = Github(os.environ['ACC_TOKEN_SWENG_GH'])
 repos = g.get_user().get_repos()
 i = 1
+commList = []
 for repo in repos:
     print("Repo: " + str(i) + " of " + str(repos.totalCount))
     i += 1
-    # keyKey = (list(repo.get_branches())[0].commit).sha
-    # print(repo.get_commit(keyKey).commit.author.date)
-    # print(list(repo.get_contributors()))
+
     authList = list(repo.get_contributors())
     if authList[0].login == "dartse" and len(authList) == 1:
-        # print(authList)
+
         commitList = repo.get_commits()
         j = 1
         for commit in commitList:
             print("Commit: " + str(j) + " of " + str(commitList.totalCount))
             j+=1
-            f1.write(str(repo.get_commit(commit.sha).commit.author.date)+ "\n")
-            # print(commit)
-            # print(repo.get_commit(commit.sha).commit.author.date)
+            commList.append(repo.get_commit(commit.sha).commit.author.date)
+
+commList.sort()
+for date in commList:
+    f1.write(str(date)+"\n")
 f1.close
