a84e17c7a43c0feaefca72f52729dc1426d2faef
darts <darts@tcd.ie>
2019-10-29 18:54:20 +0000

    Update acc.py

--- access/acc.py
+++ access/acc.py
@@ -1,7 +1,19 @@
 from github import Github
 import os
 
-g = Github(os.environ['ACC_TOKEN_SWENG_GH'])
+f1 = open('commits.txt', 'w+')
 
-for repo in g.get_user().get_repos():
-    print(repo.full_name)
+g = Github(os.environ['ACC_TOKEN_SWENG_GH'])
+repos = g.get_user().get_repos()
+for repo in repos:
+    # keyKey = (list(repo.get_branches())[0].commit).sha
+    # print(repo.get_commit(keyKey).commit.author.date)
+    # print(list(repo.get_contributors()))
+    authList = list(repo.get_contributors())
+    if authList[0].login == "dartse" and len(authList) == 1:
+        print(authList)
+    #     commitList = g.get_user().get_repos()[0].get_commits()
+    #     for commit in commitList:
+    #         # f1.writelines()
+    #         print(commit.author)
+    #         # print(repo.get_commit(commit.sha).commit.author.date)
