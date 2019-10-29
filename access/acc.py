772de2bbb1e2ec70bce48970d38392a8e76e15d9
darts <darts@tcd.ie>
2019-10-29 21:53:35 +0000

    Date stuffs and bash args

--- access/acc.py
+++ access/acc.py
@@ -1,8 +1,14 @@
 from github import Github
 import os
+import sys
+
+if len(sys.argv) != 2:
+    print(f'usage: {sys.argv[0]} <openType>\n       opentype: 0 = use existing dates\n                 1 = get fresh dates\n                 3 = *experimental* be smart and get only latest commits')
+    exit(1)
+
+theF = open('commits.txt', 'r')
 
 f1 = open('commits.txt', 'w+')
-
 g = Github(os.environ['ACC_TOKEN_SWENG_GH'])
 repos = g.get_user().get_repos()
 i = 1
