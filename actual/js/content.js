11ab62e80eb2edee86eae73568223341c50759e4
darts <darts@tcd.ie>
2019-11-19 17:55:05 +0000

    Some progress

--- actual/js/content.js
+++ actual/js/content.js
@@ -1,10 +1,46 @@
-Gh3.Users.search("mad", {start_page : 3}, function (err, response) {
-	if(err) {
-		throw "outch ..."
-	}
-	//console.log(Gh3.Users.getAll());
-	console.log(response.getAll());
-	response.each(function (user) {
-		console.log(user.name, user.login, user.repos, user)
-	});
+// var input = window.prompt("Pick a user:", "dartse");
+
+var usr_ = new Gh3.User("dartse");
+var usr_repos = new Gh3.Repositories(usr_);
+// console.log(usr_repos);
+usr_repos.fetch({page:1, per_page:500, direction : "desc"}, "next", function(err, response){
+    if(err){
+        throw "seems like a you problem..."
+    }
+    console.log(usr_repos);
 });
+
+// k33g.fetch(function (err, resUser) {
+//     if (err) {
+//         throw "outch ..."
+//     }
+//     console.log(k33g, resUser);
+//     _.each(_.keys(resUser), function (prop) {
+//         userInfos.append(
+//             $('<li>').append(prop + " : " + resUser[prop])
+//         );
+//     });
+// });
+
+// let data = [];
+// Gh3.Users.search("mad", { start_page: 3 }, function (err, response) {
+//     if (err) {
+//         throw "outch ..."
+//     }
+//     response.each(function (user) {
+//         data.push(user.public_repo_count)
+//     });
+//     var x_axis = [];
+//     for (var i = 0; i < 100; i++) {
+//         x_axis.push(i);
+//     }
+//     var y_axis = data;
+//     var trace1 = {
+//         x: x_axis,
+//         y: y_axis,
+//         type: 'scatter'
+//     };
+//     Plotly.newPlot('myDiv', [trace1]);
+// });
+
+
