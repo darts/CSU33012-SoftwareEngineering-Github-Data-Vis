3f83414f015edf7729c51bf18d47d7f98e398530
darts <darts@tcd.ie>
2019-11-20 12:34:09 +0000

    Switch to Octokit

--- actual/js/content.js
+++ actual/js/content.js
@@ -1,14 +1,75 @@
+var input = window.prompt("Pick a user:", "dartse");
+
+const octokit = Octokit({
+    userAgent: 'myApp v1.2.3'
+});
+
+let thePromise = octokit.users.getByUsername({ username: input });
+thePromise.then(function (result) {
+    console.log(result); // "Stuff worked!"
+}, function (err) {
+    console.log(err); // Error: "It broke"
+});
+
+let pp = octokit.repos.getPunchCardStats({
+    owner: input,
+    repo: "CSU34011-SymbolicProgramming"
+})
+
+pp.then(function (result) {
+    console.log(result);
+}, function (err) { 
+    console.log(err); 
+})
+
+let ppp = octokit.repos.listForUser({
+    username: input
+})
+
+ppp.then(function(result){
+    console.log(result);
+}, function(err){
+    console.log(result);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // var input = window.prompt("Pick a user:", "dartse");
 
-var usr_ = new Gh3.User("dartse");
-var usr_repos = new Gh3.Repositories(usr_);
-// console.log(usr_repos);
-usr_repos.fetch({page:1, per_page:500, direction : "desc"}, "next", function(err, response){
-    if(err){
-        throw "seems like a you problem..."
-    }
-    console.log(usr_repos);
-});
+// var usr_ = new Gh3.User("dartse");
+// var usr_repos = new Gh3.Repositories(usr_);
+// // console.log(usr_repos);
+// usr_repos.fetch({page:1, per_page:500, direction : "desc"}, "next", function(err, response){
+//     if(err){
+//         throw "seems like a you problem..."
+//     }
+//     console.log(usr_repos.repositories);
+// });
+
+// let gh = new GitHub();
+// let g = gh.getGist();
+// let me = gh.getUser('dartse');
+// me.listRepos({}, function(err, response){
+//     if(err){
+//         throw "could not list repos"
+//     }
+//     reResponse = response[0].
+//     console.log(response);
+// });
+// console.log(clayreimann);
 
 // k33g.fetch(function (err, resUser) {
 //     if (err) {
