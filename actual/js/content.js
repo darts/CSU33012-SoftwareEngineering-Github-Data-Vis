22d29977945672bfacc19eb237ec67a6878df8a9
darts <darts@tcd.ie>
2019-11-20 13:27:23 +0000

    Update content.js

--- actual/js/content.js
+++ actual/js/content.js
@@ -4,104 +4,59 @@ const octokit = Octokit({
     userAgent: 'myApp v1.2.3'
 });
 
-let thePromise = octokit.users.getByUsername({ username: input });
-thePromise.then(function (result) {
-    console.log(result); // "Stuff worked!"
-}, function (err) {
-    console.log(err); // Error: "It broke"
-});
+// ********************
+// get generic user data
+// let thePromise = octokit.users.getByUsername({ username: input });
+// thePromise.then(function (result) {
+//     console.log(result); // "Stuff worked!"
+// }, function (err) {
+//     console.log(err); // Error: "It broke"
+// });
 
-let pp = octokit.repos.getPunchCardStats({
-    owner: input,
-    repo: "CSU34011-SymbolicProgramming"
-})
 
-pp.then(function (result) {
-    console.log(result);
-}, function (err) { 
-    console.log(err); 
-})
-
+// ********************
+// get commit time data
 let ppp = octokit.repos.listForUser({
     username: input
 })
 
-ppp.then(function(result){
+ppp.then(function (result) {
+    // console.log(result.data);
+    result.data.forEach(dankPrintRepo);
+    dankGetLangDeets(result.data[0], 0);
+    // result.data.forEach(dankGetLangDeets);
     console.log(result);
-}, function(err){
-    console.log(result);
+}, function (err) {
+    console.log(err);
 })
 
 
+let dankPrintRepo = function (repo, index) {
+    let pp = octokit.repos.getPunchCardStats({
+        owner: input,
+        repo: repo
+    })
 
+    pp.then(function (result) {
+        // console.log(result);
+    }, function (err) {
+        console.log(err);
+    })
+}
 
+// ********************
+// Get lang stats
 
+let dankGetLangDeets = function(repo, index){
+    let pp = octokit.repos.listLanguages({
+        owner: input,
+        repo: repo
+    })
 
-
-
-
-
-
-
-
-
-
-
-// var input = window.prompt("Pick a user:", "dartse");
-
-// var usr_ = new Gh3.User("dartse");
-// var usr_repos = new Gh3.Repositories(usr_);
-// // console.log(usr_repos);
-// usr_repos.fetch({page:1, per_page:500, direction : "desc"}, "next", function(err, response){
-//     if(err){
-//         throw "seems like a you problem..."
-//     }
-//     console.log(usr_repos.repositories);
-// });
-
-// let gh = new GitHub();
-// let g = gh.getGist();
-// let me = gh.getUser('dartse');
-// me.listRepos({}, function(err, response){
-//     if(err){
-//         throw "could not list repos"
-//     }
-//     reResponse = response[0].
-//     console.log(response);
-// });
-// console.log(clayreimann);
-
-// k33g.fetch(function (err, resUser) {
-//     if (err) {
-//         throw "outch ..."
-//     }
-//     console.log(k33g, resUser);
-//     _.each(_.keys(resUser), function (prop) {
-//         userInfos.append(
-//             $('<li>').append(prop + " : " + resUser[prop])
-//         );
-//     });
-// });
-
-// let data = [];
-// Gh3.Users.search("mad", { start_page: 3 }, function (err, response) {
-//     if (err) {
-//         throw "outch ..."
-//     }
-//     response.each(function (user) {
-//         data.push(user.public_repo_count)
-//     });
-//     var x_axis = [];
-//     for (var i = 0; i < 100; i++) {
-//         x_axis.push(i);
-//     }
-//     var y_axis = data;
-//     var trace1 = {
-//         x: x_axis,
-//         y: y_axis,
-//         type: 'scatter'
-//     };
-//     Plotly.newPlot('myDiv', [trace1]);
-// });
-
+    pp.then(function(result){
+        console.log(result);
+    }, function (err){
+        console.log(err);
+    })
+}
 
