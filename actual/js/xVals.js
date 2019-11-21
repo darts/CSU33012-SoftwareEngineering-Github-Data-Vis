5bb5ab57baa9103b80f13db5e4c3bca1a4e1cd96
darts <darts@tcd.ie>
2019-11-21 13:01:25 +0000

    Now pulling nice lang data

--- actual/js/xVals.js
+++ actual/js/xVals.js
@@ -36,7 +36,4 @@ let genPairs = function(length){
     return dubArr([...Array(length).keys()])
 }
 
-//takes an array of length 168 and splits it into 7
-let splitVals = function(arrayLen168){
 
-}
