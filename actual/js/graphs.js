f94a96ab8bac3e5beccdc7bf28c73083063f4191
darts <darts@tcd.ie>
2019-11-26 01:55:58 +0000

    Graph fix and beautify
    Ribbon graph is now properly sized.
    (Almost) everything is now in CSS grids.
    Card all the things.
    Swap button is now nicer-looking and animated.
    Highlights on mousover of cards.

--- actual/js/graphs.js
+++ actual/js/graphs.js
@@ -1,6 +1,6 @@
 let env = {}
 let drawCommitTimeGraphs = function (theArr, names, divName) {
-    document.getElementById(divName).innerHTML = ""
+    // document.getElementById(divName).innerHTML = ""
     let xArr = buildTimes()
     let traceArr = []
     theArr.forEach((set, index) => {
@@ -15,10 +15,12 @@ let drawCommitTimeGraphs = function (theArr, names, divName) {
         }
         traceArr.push(trace)
     })
-    let layout = {title: 'Commits by day and time',
-                  xaxis: { title: "Time of day (24hr)" }, 
-                  yaxis: { title: "Number of commits" } };
-    Plotly.newPlot(divName, traceArr, layout);
+    let layout = {
+        title: 'Commits by day and time',
+        xaxis: { title: "Time of day (24hr)" },
+        yaxis: { title: "Number of commits" }
+    };
+    Plotly.newPlot(divName, traceArr, layout, { displayModeBar: false });
 }
 
 let drawCommitTimeRibbon = function (theArr, names, divName) {
@@ -27,7 +29,7 @@ let drawCommitTimeRibbon = function (theArr, names, divName) {
     let xArr = buildTimes()
     let traceArr = []
 
-    
+
 
     theArr.forEach((set, index) => {
         // let anArr = new Array(168).fill(index)
@@ -44,13 +46,17 @@ let drawCommitTimeRibbon = function (theArr, names, divName) {
         }
         traceArr.push(trace)
     })
-    let layout = { 
+    let layout = {
         showlegend: false,
-        title: 'Commits by day and time',
-        scene: {xaxis: { title: "Time of day (24hr)" }, 
-                yaxis: { title: "Day of the week (Sun - Sat)" },
-                zaxis: { title: "Number of commits" } } };
-    Plotly.newPlot(divName, traceArr, layout);
+        autosize: true,
+        margin: { "l": 0, "r": 0, "b": 0, "t": 0 },
+        scene: {
+            xaxis: { title: "Time of day (24hr)" },
+            yaxis: { title: "Day of the week (Sun - Sat)" },
+            zaxis: { title: "Number of commits" }
+        }
+    };
+    Plotly.newPlot(divName, traceArr, layout, { displayModeBar: false });
 }
 
 let drawLangPie = function (langStats, repoNames) {
@@ -70,59 +76,59 @@ let drawLangPie = function (langStats, repoNames) {
     var layout = {
         "margin": { "l": 0, "r": 0, "b": 0, "t": 0 },
     };
-    Plotly.newPlot('langSunBurst', data, layout)
+    Plotly.newPlot('langSunBurst', data, layout, { displayModeBar: false })
 }
 
-let drawBarChartsShort = function(repoName){
-    drawBarCharts(repoName, fillScrollBarDates(repoName, namesS,0).date)
+let drawBarChartsShort = function (repoName) {
+    drawBarCharts(repoName, fillScrollBarDates(repoName, namesS, 0).date)
 }
 
-let drawBarCharts = function(repoName, endDate){
+let drawBarCharts = function (repoName, endDate) {
 
-    let a = getValsStartingAt(repoName,preParsedValues[repoName], endDate)
+    let a = getValsStartingAt(repoName, preParsedValues[repoName], endDate)
     var trace1 = {
         x: a.dates,
         y: a.addList,
         type: 'bar',
         name: 'Additions',
         marker: {
-          color: 'rgb(49,130,189)',
-          opacity: 0.7,
+            color: 'rgb(49,130,189)',
+            opacity: 0.7,
         }
-      };
-      
-      var trace2 = {
+    };
+
+    var trace2 = {
         x: a.dates,
         y: a.delList,
         type: 'bar',
         name: 'Deletions',
         marker: {
-          color: 'rgb(204,204,204)',
-          opacity: 0.5
+            color: 'rgb(204,204,204)',
+            opacity: 0.5
         }
-      };
-      
-      var data = [trace1, trace2];
-      
-      var layout = {
+    };
+
+    var data = [trace1, trace2];
+
+    var layout = {
         title: 'Additions vs Deletions',
         xaxis: {
-          tickangle: -45
+            tickangle: -45
         },
         barmode: 'group'
-      };
-      
-      Plotly.newPlot('churn', data, layout,{ responsive: true });
+    };
+
+    Plotly.newPlot('churn', data, layout, { responsive: true, displayModeBar: false });
 }
 
-let updateUserOnPage = function(data){
-    document.getElementById("userData").innerHTML = `<img src="${data.avatar_url}" class="col-lg-11 col-md-11" style="margin:auto; margin-top:10px ;padding=10px 10px 10px 10px; border-radius:15%; width:100%;"></img>
-    Followers: ${data.followers}  \n
-    Following: ${data.following}  \n
-    Public Repos: ${data.public_repos}  \n
-    Hireable: ${data.hireable == null ? "Not published" : data.hireable}  \n
-    Username:${data.login}  \n
-    Name: ${data.name == null ? "Not published" : data.name}  \n
-    Bio: ${data.bio}  \n
+let updateUserOnPage = function (data) {
+    document.getElementById("userData").innerHTML = `<img src="${data.avatar_url}" style="margin-top:0px ;padding=1px 1px 1px 1px; border-radius:5px; width:95%; align:left"></img><br/>
+    Followers: ${data.followers}  \n<br/>
+    Following: ${data.following}  \n<br/>
+    Public Repos: ${data.public_repos}  \n<br/>
+    Hireable: ${data.hireable == null ? "Not published" : data.hireable}  \n<br/>
+    Username:${data.login}  \n<br/>
+    Name: ${data.name == null ? "Not published" : data.name}  \n<br/>
+    Bio: ${data.bio}  \n<br/>
     Location: ${data.location}  \n`
 }
